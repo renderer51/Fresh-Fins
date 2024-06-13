382b9a55321c6802ef2feccab7d6a51c68a39ed8
renderer51 <navneethkrishnak.iet@srinivasuniversity.edu.in>
2024-06-13 23:53:17 +0530

    Added Navigation via Java

--- app/src/main/java/com/example/freshfins/MainActivity.java
+++ app/src/main/java/com/example/freshfins/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.freshfins;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    TextView forgot_password, register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        forgot_password = findViewById(R.id.forgot_password);
+        register = findViewById(R.id.registration);
+
+       /* forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ForgotPassword.class);
+                startActivity(intent);
+            }
+        });*/
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/freshfins/Register.java
+++ app/src/main/java/com/example/freshfins/Register.java
@@ -1,13 +1,35 @@
 package com.example.freshfins;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Register extends AppCompatActivity {
+    Button fishermanRegistration, userRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        fishermanRegistration = findViewById(R.id.fisherman_reg);
+        userRegistration = findViewById(R.id.customer_reg);
+
+        fishermanRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, FishermanRegistration.class);
+                startActivity(intent);
+            }
+        });
+
+        userRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Register.this, UserRegistration.class);
+                startActivity(intent);
+            }
+        });
     }
 }
