e3b719829096cb6557626c7dad041881308a7d00
renderer51 <navneethkrishnak.iet@srinivasuniversity.edu.in>
2024-06-14 21:24:35 +0530

    Home Page works with test data

--- app/src/main/java/com/example/freshfins/MainActivity.java
+++ app/src/main/java/com/example/freshfins/MainActivity.java
@@ -2,12 +2,16 @@ package com.example.freshfins;
 
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
     TextView forgot_password, register;
+    TextInputLayout phone_no, password;
+    Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         forgot_password = findViewById(R.id.forgot_password);
         register = findViewById(R.id.registration);
+        phone_no = findViewById(R.id.phone_number);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login_btn);
 
        /* forgot_password.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Register.class);
                 startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(phone_no.getEditText().getText().toString().equals("9980174900") && password.getEditText().getText().toString().equals("123456")){
+                    Intent intent = new Intent(MainActivity.this, HomePage.class);
+                    startActivity(intent);
+                }
             }
         });
     }
